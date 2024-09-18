a1236e60110ee8944878e26cfd29067098a21d29
chillinwithyou <datextre@gmail.com>
2024-09-18 03:14:02 -0400

    Example for proper use

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Consumer;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('consumers', function () {
+Route::post('consumers', function (Request $request) {
     // validation...
 
-    Consumer::create([
+    /*Consumer::create([
         'first_name' => request('first_name'),
         'last_name' => request('last_name'),
         'email' => request('email'),
     ]);
+    */
 
-    return redirect('/');
+    $validated = $request->validate([
+        "email" => ['email', 'required'],
+    ]);
+
+    $validated["email"] = urlencode($validated["email"]);
+
+    return redirect()->to("https://buy.stripe.com/3cseWD7awdlW57W5kv?prefilled_email={$validated["email"]}");
+});
+
+
+Route::get("/testing/{email}", function (String $email){
+    
 });
