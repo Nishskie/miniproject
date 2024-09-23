1fdf7973e0c4e2737008cecf647656215d6b17a2
Nishskie <nishiiiiiafiq@gmail.com>
2024-09-22 21:07:55 -0400

    test4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base font-semibold leading-7 text-gray-900">Email</h2>
+                <h2 class="text-base font-semibold leading-7 text-gray-900">Email!</h2>
                 <p class="mt-1 text-sm leading-6 text-gray-600">Fill in the your email for a bonus</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
